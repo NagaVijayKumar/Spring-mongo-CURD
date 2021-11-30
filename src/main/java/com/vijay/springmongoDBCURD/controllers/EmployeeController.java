317f8e729af467e5639c06c578ec769d8fbc50d6
Vijay Kumar <vijaykumarcseiiit@gmail.com>
2021-11-30 08:20:40 +0530

    Create Emp Controller added

--- src/main/java/com/vijay/springmongoDBCURD/controllers/EmployeeController.java
+++ src/main/java/com/vijay/springmongoDBCURD/controllers/EmployeeController.java
@@ -1,14 +1,30 @@
 package com.vijay.springmongoDBCURD.controllers;
 
+import com.vijay.springmongoDBCURD.models.Employee;
+import com.vijay.springmongoDBCURD.repository.EmployeeRepository;
+import com.vijay.springmongoDBCURD.service.SequenceGeneratorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
+
 
     // Create
     @PostMapping(path = "employees")
+    public Employee createEmployee(@Valid @RequestBody Employee employee) {
+        employee.setId(sequenceGeneratorService.generateSequence(Employee.SEQUENCE_NAME));
+        return employeeRepository.save(employee);
+    }
     public String CreateEmployee(){
         return "Employee Created Successfully";
     }
